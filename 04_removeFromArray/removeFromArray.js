84ace6c01e3110b0fd5dffff24395274ee195fce
Jerish-J <jjerishj@gmail.com>
2022-10-02 20:36:08 +0530

    exercise 4 test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(val => !args.includes(val))
 };
 
-// Do not edit below this line
+// Do not edit below this linex
 module.exports = removeFromArray;
